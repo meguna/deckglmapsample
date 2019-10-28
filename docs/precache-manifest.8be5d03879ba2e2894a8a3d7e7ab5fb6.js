69b609d2df70acbc9b17ec040072899bd29093b8
Meg <megunaokawa@gmail.com>
2019-10-28 16:03:54 -0400

    new build folder

---
+++ docs/precache-manifest.8be5d03879ba2e2894a8a3d7e7ab5fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8288746bff1330b30c400e4200e05fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e264f8beab9ea93558d",
+    "url": "/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "2327d79293545a069b15",
+    "url": "/static/css/main.4ff4e576.chunk.css"
+  },
+  {
+    "revision": "3e264f8beab9ea93558d",
+    "url": "/static/js/2.36ee69c7.chunk.js"
+  },
+  {
+    "revision": "2327d79293545a069b15",
+    "url": "/static/js/main.8036fbb4.chunk.js"
+  },
+  {
+    "revision": "ed362e0bc57b984468bb",
+    "url": "/static/js/runtime-main.4dc73999.js"
+  }
+]);
